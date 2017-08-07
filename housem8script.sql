4de09b56e446840e70963740b8519c9edf515f82
mig21bb <mig21bb@gmail.com>
2017-08-08 00:03:20 +0200

    sql script

--- housem8script.sql
+++ housem8script.sql
@@ -7,231 +7,145 @@ SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
 -- -----------------------------------------------------
 -- Schema housem8_schema
 -- -----------------------------------------------------
+CREATE DATABASE `housem8_schema` /*!40100 DEFAULT CHARACTER SET utf8 COLLATE utf8_spanish_ci */;
 
--- -----------------------------------------------------
--- Schema housem8_schema
--- -----------------------------------------------------
-CREATE SCHEMA IF NOT EXISTS `housem8_schema` DEFAULT CHARACTER SET utf8 COLLATE utf8_latvian_ci ;
-USE `housem8_schema` ;
-
--- -----------------------------------------------------
--- Table `housem8_schema`.`HOUSE`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `housem8_schema`.`HOUSE` ;
-
-CREATE TABLE IF NOT EXISTS `housem8_schema`.`HOUSE` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `country` VARCHAR(45) NULL,
-  `city` VARCHAR(45) NOT NULL,
-  `street` VARCHAR(45) NOT NULL,
-  `cp` VARCHAR(45) NULL,
-  `number` INT NULL,
-  `floor` INT NULL,
-  `apartment` VARCHAR(45) NULL,
-  `other` VARCHAR(45) NULL,
-  `square_meters` FLOAT NOT NULL,
-  PRIMARY KEY (`id`))
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
--- Table `housem8_schema`.`ROOM_CLASS`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `housem8_schema`.`ROOM_CLASS` ;
-
-CREATE TABLE IF NOT EXISTS `housem8_schema`.`ROOM_CLASS` (
-  `id` INT NOT NULL,
-  `description` VARCHAR(200) NOT NULL,
-  `category` VARCHAR(45) NOT NULL,
-  PRIMARY KEY (`id`))
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
--- Table `housem8_schema`.`ROOM`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `housem8_schema`.`ROOM` ;
-
-CREATE TABLE IF NOT EXISTS `housem8_schema`.`ROOM` (
-  `id` INT NOT NULL,
-  `square_meters` FLOAT NOT NULL,
-  `windows` TINYINT(1) NULL,
-  `HOUSE_id` INT NOT NULL,
-  `ROOM_CLASS_id` INT NOT NULL,
+CREATE TABLE `compensation` (
+  `id` int(11) NOT NULL,
+  `amount` float NOT NULL,
+  `datetime` datetime NOT NULL,
+  `payer` int(11) NOT NULL,
+  `receiver` int(11) NOT NULL,
+  `notes` longtext COLLATE utf8_spanish_ci,
+  `activo` tinyint(1) NOT NULL DEFAULT '1',
+  `fecha_creacion` datetime NOT NULL,
+  `fecha_modificacion` datetime DEFAULT NULL,
+  `fecha_borrado` datetime DEFAULT NULL,
   PRIMARY KEY (`id`),
-  INDEX `fk_ROOM_HOUSE_idx` (`HOUSE_id` ASC),
-  INDEX `fk_ROOM_ROOM_CLASS1_idx` (`ROOM_CLASS_id` ASC),
-  CONSTRAINT `fk_ROOM_HOUSE`
-    FOREIGN KEY (`HOUSE_id`)
-    REFERENCES `housem8_schema`.`HOUSE` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_ROOM_ROOM_CLASS1`
-    FOREIGN KEY (`ROOM_CLASS_id`)
-    REFERENCES `housem8_schema`.`ROOM_CLASS` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+  KEY `fk_COMPENSATION_MATE1_idx` (`payer`),
+  KEY `fk_COMPENSATION_MATE2_idx` (`receiver`),
+  CONSTRAINT `fk_COMPENSATION_MATE1` FOREIGN KEY (`payer`) REFERENCES `mate` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `fk_COMPENSATION_MATE2` FOREIGN KEY (`receiver`) REFERENCES `mate` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
 
-
--- -----------------------------------------------------
--- Table `housem8_schema`.`MATE`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `housem8_schema`.`MATE` ;
-
-CREATE TABLE IF NOT EXISTS `housem8_schema`.`MATE` (
-  `id` INT NOT NULL,
-  `name` VARCHAR(45) NULL,
-  `surname1` VARCHAR(45) NULL,
-  `surname2` VARCHAR(45) NULL,
-  `birth_date` DATE NULL,
-  `nationality` VARCHAR(45) NULL,
-  `email` VARCHAR(45) NULL,
-  `user` VARCHAR(45) NULL,
-  `pass` VARCHAR(45) NULL,
-  PRIMARY KEY (`id`))
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
--- Table `housem8_schema`.`OCUPATION`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `housem8_schema`.`OCUPATION` ;
-
-CREATE TABLE IF NOT EXISTS `housem8_schema`.`OCUPATION` (
-  `id` INT NOT NULL,
-  `ROOM_id` INT NOT NULL,
-  `MATE_id` INT NOT NULL,
-  `start_date` DATE NOT NULL,
-  `end_date` DATE NULL,
+CREATE TABLE `cost` (
+  `id` int(11) NOT NULL,
+  `description` varchar(45) COLLATE utf8_spanish_ci DEFAULT NULL,
+  `period` int(11) DEFAULT NULL,
+  `datetime` datetime DEFAULT NULL,
+  `amount` float DEFAULT NULL,
+  `MATE_id` int(11) NOT NULL,
+  `HOUSE_id` int(11) NOT NULL,
+  `COST_FAMILY_id` int(11) NOT NULL,
+  `activo` tinyint(1) NOT NULL DEFAULT '1',
+  `fecha_creacion` datetime NOT NULL,
+  `fecha_modificacion` datetime DEFAULT NULL,
+  `fecha_borrado` datetime DEFAULT NULL,
   PRIMARY KEY (`id`),
-  INDEX `fk_OCUPATION_ROOM1_idx` (`ROOM_id` ASC),
-  INDEX `fk_OCUPATION_MATE1_idx` (`MATE_id` ASC),
-  CONSTRAINT `fk_OCUPATION_ROOM1`
-    FOREIGN KEY (`ROOM_id`)
-    REFERENCES `housem8_schema`.`ROOM` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_OCUPATION_MATE1`
-    FOREIGN KEY (`MATE_id`)
-    REFERENCES `housem8_schema`.`MATE` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+  KEY `fk_COST_MATE1_idx` (`MATE_id`),
+  KEY `fk_COST_HOUSE1_idx` (`HOUSE_id`),
+  KEY `fk_COST_COST_FAMILY1_idx` (`COST_FAMILY_id`),
+  CONSTRAINT `fk_COST_COST_FAMILY1` FOREIGN KEY (`COST_FAMILY_id`) REFERENCES `cost_family` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `fk_COST_HOUSE1` FOREIGN KEY (`HOUSE_id`) REFERENCES `house` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `fk_COST_MATE1` FOREIGN KEY (`MATE_id`) REFERENCES `mate` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
 
+CREATE TABLE `cost_family` (
+  `id` int(11) NOT NULL,
+  `name` varchar(45) COLLATE utf8_spanish_ci NOT NULL,
+  `description` varchar(45) COLLATE utf8_spanish_ci NOT NULL,
+  `period` int(11) DEFAULT NULL,
+  `activo` tinyint(1) NOT NULL DEFAULT '1',
+  `fecha_creacion` datetime NOT NULL,
+  `fecha_modificacion` datetime DEFAULT NULL,
+  `fecha_borrado` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
 
--- -----------------------------------------------------
--- Table `housem8_schema`.`COST_FAMILY`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `housem8_schema`.`COST_FAMILY` ;
+CREATE TABLE `house` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `country` varchar(45) COLLATE utf8_spanish_ci DEFAULT NULL,
+  `city` varchar(45) COLLATE utf8_spanish_ci NOT NULL,
+  `street` varchar(45) COLLATE utf8_spanish_ci NOT NULL,
+  `cp` varchar(45) COLLATE utf8_spanish_ci DEFAULT NULL,
+  `number` int(11) DEFAULT NULL,
+  `floor` int(11) DEFAULT NULL,
+  `apartment` varchar(45) COLLATE utf8_spanish_ci DEFAULT NULL,
+  `other` varchar(45) COLLATE utf8_spanish_ci DEFAULT NULL,
+  `square_meters` float NOT NULL,
+  `activo` tinyint(1) NOT NULL DEFAULT '1',
+  `fecha_creacion` datetime NOT NULL,
+  `fecha_modificacion` datetime DEFAULT NULL,
+  `fecha_borrado` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
 
-CREATE TABLE IF NOT EXISTS `housem8_schema`.`COST_FAMILY` (
-  `id` INT NOT NULL,
-  `name` VARCHAR(45) NOT NULL,
-  `description` VARCHAR(45) NOT NULL,
-  `period` INT NULL,
-  PRIMARY KEY (`id`))
-ENGINE = InnoDB;
+CREATE TABLE `mate` (
+  `id` int(11) NOT NULL,
+  `name` varchar(45) COLLATE utf8_spanish_ci DEFAULT NULL,
+  `surname1` varchar(45) COLLATE utf8_spanish_ci DEFAULT NULL,
+  `surname2` varchar(45) COLLATE utf8_spanish_ci DEFAULT NULL,
+  `birth_date` date DEFAULT NULL,
+  `nationality` varchar(45) COLLATE utf8_spanish_ci DEFAULT NULL,
+  `email` varchar(45) COLLATE utf8_spanish_ci DEFAULT NULL,
+  `user` varchar(45) COLLATE utf8_spanish_ci DEFAULT NULL,
+  `pass` varchar(45) COLLATE utf8_spanish_ci DEFAULT NULL,
+  `activo` tinyint(1) NOT NULL DEFAULT '1',
+  `fecha_creacion` datetime NOT NULL,
+  `fecha_modificacion` datetime DEFAULT NULL,
+  `fecha_borrado` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
 
-
--- -----------------------------------------------------
--- Table `housem8_schema`.`COST`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `housem8_schema`.`COST` ;
-
-CREATE TABLE IF NOT EXISTS `housem8_schema`.`COST` (
-  `id` INT NOT NULL,
-  `description` VARCHAR(45) NULL,
-  `period` INT NULL,
-  `datetime` DATETIME NULL,
-  `amount` FLOAT NULL,
-  `MATE_id` INT NOT NULL,
-  `HOUSE_id` INT NOT NULL,
-  `COST_FAMILY_id` INT NOT NULL,
+CREATE TABLE `ocupation` (
+  `id` int(11) NOT NULL,
+  `ROOM_id` int(11) NOT NULL,
+  `MATE_id` int(11) NOT NULL,
+  `start_date` date NOT NULL,
+  `end_date` date DEFAULT NULL,
+  `activo` tinyint(1) NOT NULL DEFAULT '1',
+  `fecha_creacion` datetime NOT NULL,
+  `fecha_modificacion` datetime DEFAULT NULL,
+  `fecha_borrado` datetime DEFAULT NULL,
   PRIMARY KEY (`id`),
-  INDEX `fk_COST_MATE1_idx` (`MATE_id` ASC),
-  INDEX `fk_COST_HOUSE1_idx` (`HOUSE_id` ASC),
-  INDEX `fk_COST_COST_FAMILY1_idx` (`COST_FAMILY_id` ASC),
-  CONSTRAINT `fk_COST_MATE1`
-    FOREIGN KEY (`MATE_id`)
-    REFERENCES `housem8_schema`.`MATE` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_COST_HOUSE1`
-    FOREIGN KEY (`HOUSE_id`)
-    REFERENCES `housem8_schema`.`HOUSE` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_COST_COST_FAMILY1`
-    FOREIGN KEY (`COST_FAMILY_id`)
-    REFERENCES `housem8_schema`.`COST_FAMILY` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+  KEY `fk_OCUPATION_ROOM1_idx` (`ROOM_id`),
+  KEY `fk_OCUPATION_MATE1_idx` (`MATE_id`),
+  CONSTRAINT `fk_OCUPATION_MATE1` FOREIGN KEY (`MATE_id`) REFERENCES `mate` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `fk_OCUPATION_ROOM1` FOREIGN KEY (`ROOM_id`) REFERENCES `room` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
 
+CREATE TABLE `pictures` (
+  `id` int(11) NOT NULL,
+  `url` varchar(100) COLLATE utf8_spanish_ci DEFAULT NULL,
+  `datetime` datetime DEFAULT NULL,
+  `active` binary(1) DEFAULT '1',
+  `data_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
 
--- -----------------------------------------------------
--- Table `housem8_schema`.`COMPENSATION`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `housem8_schema`.`COMPENSATION` ;
-
-CREATE TABLE IF NOT EXISTS `housem8_schema`.`COMPENSATION` (
-  `id` INT NOT NULL,
-  `amount` FLOAT NOT NULL,
-  `datetime` DATETIME NOT NULL,
-  `payer` INT NOT NULL,
-  `receiver` INT NOT NULL,
-  `notes` LONGTEXT NULL,
+CREATE TABLE `room` (
+  `id` int(11) NOT NULL,
+  `square_meters` float NOT NULL,
+  `windows` tinyint(1) DEFAULT NULL,
+  `HOUSE_id` int(11) NOT NULL,
+  `ROOM_CLASS_id` int(11) NOT NULL,
+  `activo` tinyint(1) NOT NULL DEFAULT '1',
+  `fecha_creacion` datetime NOT NULL,
+  `fecha_modificacion` datetime DEFAULT NULL,
+  `fecha_borrado` datetime DEFAULT NULL,
   PRIMARY KEY (`id`),
-  INDEX `fk_COMPENSATION_MATE1_idx` (`payer` ASC),
-  INDEX `fk_COMPENSATION_MATE2_idx` (`receiver` ASC),
-  CONSTRAINT `fk_COMPENSATION_MATE1`
-    FOREIGN KEY (`payer`)
-    REFERENCES `housem8_schema`.`MATE` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_COMPENSATION_MATE2`
-    FOREIGN KEY (`receiver`)
-    REFERENCES `housem8_schema`.`MATE` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+  KEY `fk_ROOM_HOUSE_idx` (`HOUSE_id`),
+  KEY `fk_ROOM_ROOM_CLASS1_idx` (`ROOM_CLASS_id`),
+  CONSTRAINT `fk_ROOM_HOUSE` FOREIGN KEY (`HOUSE_id`) REFERENCES `house` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `fk_ROOM_ROOM_CLASS1` FOREIGN KEY (`ROOM_CLASS_id`) REFERENCES `room_class` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
 
-
--- -----------------------------------------------------
--- Table `housem8_schema`.`TABLE_NAME`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `housem8_schema`.`TABLE_NAME` ;
-
-CREATE TABLE IF NOT EXISTS `housem8_schema`.`TABLE_NAME` (
-  `id` INT NOT NULL,
-  `table_name` VARCHAR(45) NULL,
-  PRIMARY KEY (`id`))
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
--- Table `housem8_schema`.`PICTURES`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `housem8_schema`.`PICTURES` ;
-
-CREATE TABLE IF NOT EXISTS `housem8_schema`.`PICTURES` (
-  `id` INT NOT NULL,
-  `url` VARCHAR(100) NULL,
-  `datetime` DATETIME NULL,
-  `active` BINARY NULL DEFAULT 1,
-  `TABLE_NAME_id` INT NOT NULL,
-  `data_id` INT NULL,
-  PRIMARY KEY (`id`),
-  INDEX `fk_PICTURES_TABLE_NAME1_idx` (`TABLE_NAME_id` ASC),
-  CONSTRAINT `fk_PICTURES_TABLE_NAME1`
-    FOREIGN KEY (`TABLE_NAME_id`)
-    REFERENCES `housem8_schema`.`TABLE_NAME` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
-
-
-SET SQL_MODE=@OLD_SQL_MODE;
-SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
-SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
+CREATE TABLE `room_class` (
+  `id` int(11) NOT NULL,
+  `description` varchar(200) COLLATE utf8_spanish_ci NOT NULL,
+  `category` varchar(45) COLLATE utf8_spanish_ci NOT NULL,
+  `activo` tinyint(1) NOT NULL DEFAULT '1',
+  `fecha_creacion` datetime NOT NULL,
+  `fecha_modificacion` datetime DEFAULT NULL,
+  `fecha_borrado` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
